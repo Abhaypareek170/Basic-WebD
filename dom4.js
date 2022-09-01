fa609a942eeb2b99a54c45fee4a28d4e377eab33
Abhay Pareek <49566872+Abhaypareek170@users.noreply.github.com>
2022-09-01 22:21:34 +0530

    Create dom4.js

---
+++ dom4.js
@@ -0,0 +1,30 @@
+// console.dir(document);
+//console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title)
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.forms)
+// console.log(document.links)
+// console.log(document.images)
+
+//getElementById
+// console.log(document.getElementById('header-title'))
+var headerTitle = document.getElementById('header-title')
+// headerTitle.textContent='Hello';
+// headerTitle.innerText='GoodBye';
+headerTitle.style.borderBottom = 'solid 3px #000'
+
+//GetElementByClassName
+// var items = document.getElementsByClassName('list-group-item')
+// console.log(items)
+// for(var i=0;i<items.length;i++){
+//     items[i].style.color = 'solid black'
+// }items[2].style.backgroundColor = 'green'
+
+//GetElementByTagName
+var items = document.getElementsByTagName('li')
+console.log(items)
+for(var i=0;i<items.length;i++){
+    items[i].style.color = 'solid black'
+}items[2].style.backgroundColor = 'green'
