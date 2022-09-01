c7918b8ce6b9f680c86a09f2be62b52eca43a143
Abhay Pareek <49566872+Abhaypareek170@users.noreply.github.com>
2022-09-01 21:55:15 +0530

    Create dom2.js
    Updated with getElementsByClassName

---
+++ dom2.js
@@ -0,0 +1,24 @@
+// console.dir(document);
+//console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title)
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.forms)
+// console.log(document.links)
+// console.log(document.images)
+
+//getElementById
+// console.log(document.getElementById('header-title'))
+var headerTitle = document.getElementById('header-title')
+// headerTitle.textContent='Hello';
+// headerTitle.innerText='GoodBye';
+headerTitle.style.borderBottom = 'solid 3px #000'
+
+//GetElementByClassName
+var items = document.getElementsByClassName('list-group-item')
+console.log(items)
+console.log(items[1])
+for(var i=0;i<items.length;i++){
+    items[i].style.backgroundColor = '#f4f4f4'
+}
